15a8241a6ac631e522b54838347af341dae2d915
Shashank Agarwal <shashank.agarwal@elastic.run>
2024-06-18 00:24:06 +0530

    removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:media_kit/media_kit.dart';
 import 'package:video_streaming/core/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  MediaKit.ensureInitialized();
+ 
   runApp(const App());
 }
